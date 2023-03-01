fd862370c03bd01e13b166af4148c0a6869a09c0
Viacheslav Privalov <privalov2306@yandex.ru>
2023-03-01 17:52:53 +0300

    Homework is done

---
+++ src/Book.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Book {
+    private String bookName;
+    private Author author;
+    private int publishingYear;
+
+    public Book(String bookName, Author author, int publishingYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+
+        return this.bookName;
+    }
+    public Author getAuthor() {
+
+        return this.author;
+    }
+    public int getPublishingYear() {
+
+        return this.publishingYear;
+    }
+    public void setPublishingYear(int publishingYear) {
+
+        this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book book = (Book) other;
+        return Objects.equals(bookName, book.bookName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(publishingYear);
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author chekhov = new Author("Антон", "Чехов");
+        Book theCherryGarden = new Book("Вишнёвый сад", chekhov, 1904);
+        System.out.println("Автор: " + theCherryGarden.getAuthor());
+        System.out.println();
+        Author rowling = new Author("Джоан", "Роулинг");
+        Book harryPotter = new Book("Harry Potter", rowling, 1997);
+        System.out.println("Автор: " + harryPotter.getAuthor());
+        System.out.println();
+        System.out.println(theCherryGarden.equals(harryPotter));
+        System.out.println(theCherryGarden.hashCode());
+        System.out.println(harryPotter.hashCode());
     }
 }
